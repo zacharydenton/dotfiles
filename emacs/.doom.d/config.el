9532ec8737e10ce1a8e52a4d1a3c38d694104e03
Zach Denton <z@chdenton.com>
2019-03-09 11:33:06 -0800

    Add emacs config.

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,27 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; UI config
+(setq doom-font (font-spec :family "xos4 Terminus" :size 18)
+      doom-variable-pitch-font (font-spec :family "Ubuntu"))
+
+;; <C-h> is backspace
+(keyboard-translate ?\C-h ?\C-?)
+(define-key evil-window-map (kbd "DEL") 'evil-window-left)
+
+;; Evil settings
+(setq evil-vsplit-window-right t)
+(setq-default evil-ex-search-highlight-all nil)
+
+;; Project settings
+(after! projectile-mode
+  (projectile-add-known-project "~/memex"))
+
+;; Jump to dired buffers
+(defun zd-rename-dired-buffer ()
+  (interactive)
+  (unless (string-match-p "Dired:" (buffer-name))
+    (rename-buffer (concat "Dired:" (buffer-name)))))
+
+(add-hook 'dired-mode-hook 'zd-rename-dired-buffer)
+(setq evil--jumps-buffer-targets "\\(\\*\\(\\new\\|scratch\\)\\*\\|Dired:.+\\)")
+(evil-add-command-properties #'dired-find-file :jump t)
--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -x
 
 git submodule update --init
+ln -s $DIR/emacs/.doom.d ~
+ln -s $DIR/emacs/.emacs.d ~
 ln -s $DIR/git/.gitconfig ~
 ln -s $DIR/tmux/.tmux.conf ~
 ln -s $DIR/vim/.vim ~
