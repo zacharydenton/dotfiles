8dbd871808c7d90949c34741e87d8fd64ad09c3f
Zach Denton <z@chdenton.com>
2019-03-09 11:53:36 -0800

    Fix emacs config.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,7 +1,7 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; UI config
-(setq doom-font (font-spec :family "xos4 Terminus" :size 18)
+(setq doom-font (font-spec :family "xos4 Terminus" :size 20)
       doom-variable-pitch-font (font-spec :family "Ubuntu"))
 
 ;; <C-h> is backspace
@@ -20,7 +20,7 @@
 (defun zd-rename-dired-buffer ()
   (interactive)
   (unless (string-match-p "Dired:" (buffer-name))
-    (rename-buffer (concat "Dired:" (buffer-name)))))
+    (rename-buffer (generate-new-buffer-name (concat "Dired:" (buffer-name))))))
 
 (add-hook 'dired-mode-hook 'zd-rename-dired-buffer)
 (setq evil--jumps-buffer-targets "\\(\\*\\(\\new\\|scratch\\)\\*\\|Dired:.+\\)")
